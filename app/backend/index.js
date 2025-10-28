7c44058b44d69ef5edd03fffccc88c08cea42bca
Adarsh6818 <adarshvish01@gmail.com>
2025-10-28 20:00:07 +0000

    Refresh API Added.

---
+++ app/backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(cors());  // Allow frontend to talk to backend
+app.use(express.json())
+
+app.get('/api/hello', (req, res) => {
+    res.json("This is code from backend")
+})
+
+app.get('/api/reset', (req, res) => {
+    res.json("")
+})
+
+app.listen(PORT, () => {
+  console.log('========================================');
+  console.log(`Example app listening on port ${PORT}`)
+  console.log(`📍 URL: http://localhost:${PORT}`);
+  console.log(`🔗 API: http://localhost:${PORT}/api/hello`);
+  console.log(`🔗 API: http://localhost:${PORT}/api/reset`);
+  console.log('========================================');
+});
